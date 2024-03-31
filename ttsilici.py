d3b13e8aee011b24c73a66767a295cfcc9cfbdf7
tereyagburak <155842501+tereyagburak@users.noreply.github.com>
2024-03-31 10:41:05 +0300

    TereyagTools Sürüm 2.0 Tam Kararlı.

--- ttsilici.py
+++
@@ -1,7 +0,0 @@
-import os
-
-os.system("del /s /q %USERPROFILE%\\TereyagTools")
-os.system("del /s /q %USERPROFILE%\\tereyagtoolscache")
-
-a = input("||||    ----Başarılı.----    ||||")
-os.startfile("https://github.com/tereyagburak/tereyagtools")
